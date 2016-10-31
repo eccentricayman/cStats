035ffbe9c9eb5d49a910ccaf274f7c7bc0e2bd5b
eccentricayman <aahmed3@stuy.edu>
2016-10-31 17:45:51 -0400

    idek

--- stat.c
+++ stat.c
@@ -1,13 +1,41 @@
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+void printHumanReadable(long long unsigned size) {
+    int ctr = 0;
+    long long unsigned sizeTemp = size;
+    while (sizeTemp) {
+        sizeTemp /= 10;
+        ctr++;
+    }
+    ctr--;
+    if (ctr < 3) {
+        printf("%llu B", size);
+    }
+    else if (ctr < 6) {
+        printf("%llu KB", size / 1000);
+    }
+    else if (ctr < 9) {
+        printf("%llu MB", size / 1000000);
+    }
+    else {
+        printf("%llu GB", size / 1000000000);
+    }
+}
 
 int main() {
-  struct stat stats;
-  stat("file", &stats);
-  printf("Size: %d\n", stats.st_size);
-  printf("Mode: %d\n", stats.st_mode);
-  printf("Time: %d\n", stats.st_atime);
-  return 0;
+    
+    struct stat stats;
+    stat("test.txt", &stats);
+    printHumanReadable(stats.st_size);
+    printf("mode: %O\n", stats.st_mode);
+    printf("last access time: %s\n", asctime(gmtime(&stats.st_atime)));
+
+    return 0;
+    
 }
